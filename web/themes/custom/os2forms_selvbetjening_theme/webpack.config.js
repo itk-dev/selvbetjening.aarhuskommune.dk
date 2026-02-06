3f4ab8918faa8626837fd3b74532e6af50ec58e6
jekuaitk <jekua@aarhus.dk>
2026-02-06 07:52:13 +0100

    6587: Place address in pdf attachment during digital post

--- web/themes/custom/os2forms_selvbetjening_theme/webpack.config.js
+++ web/themes/custom/os2forms_selvbetjening_theme/webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('pdf', './assets/pdf.js')
 
     // // will require an extra script tag for runtime.js
     // // but, you probably want this, unless you're building a single-page app
